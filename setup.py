c182541d5f5fed2d2bfd5f09ad24a25e31e116a3
Nitin Katyal <nitinkatyal1314@gmail.com>
2021-05-04 16:40:20 +0530

    added dependencied [py-ds] included

--- setup.py
+++ setup.py
@@ -1,7 +1,28 @@
 import pathlib
 from setuptools import setup, find_packages
+import re
 
 HERE = pathlib.Path(__file__).parent
+
+# helper functions to make it easier to list dependencies not as a python list, but vertically w/
+# optional built-in comments to why a certain version of the dependency is listed
+
+
+def cleanup(x):
+    return re.sub(r" *#.*", "", x.strip())  # comments
+
+
+def to_list(buffer):
+    return list(filter(None, map(cleanup, buffer.splitlines())))
+
+
+DEPENDENCIES = {
+    "core": to_list(
+      """
+        py-ds
+      """
+    )
+}
 
 VERSION = '0.1.0'
 PACKAGE_NAME = 'pyalgo'
@@ -14,7 +35,7 @@ DESCRIPTION = 'Ready to use algorithms'
 LONG_DESCRIPTION = (HERE / "README.md").read_text()
 LONG_DESC_TYPE = "text/markdown"
 
-INSTALL_REQUIRES = []
+INSTALL_REQUIRES = [y for x in DEPENDENCIES.values() for y in x]
 
 setup(name=PACKAGE_NAME,
       version=VERSION,
@@ -26,7 +47,7 @@ setup(name=PACKAGE_NAME,
       author_email=AUTHOR_EMAIL,
       url=URL,
       install_requires=INSTALL_REQUIRES,
-      python_requires=">=3.9",
+      python_requires=">=3.6",
       include_package_data=True,
       zip_safe=False,
       packages=find_packages()
